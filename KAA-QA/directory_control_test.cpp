54ee52d19d92eb25e22b3314ac1fef5a453dd1b8
Катенцев Андрей <hyperlink@inbox.ru>
2018-06-24 13:41:05 +0300

    TEST(working_directory_session, sets_and_restores_current_working_directory) added.

--- KAA-QA/directory_control_test.cpp
+++ KAA-QA/directory_control_test.cpp
@@ -6,15 +6,7 @@
 
 namespace
 {
-	/*const std::wstring kibibyte(L"./bin/KiB.bin");
-
-	/*const KAA::filesystem::driver::mode read_attributes_only(false, false);
-	const KAA::filesystem::driver::mode sequential_binary_read_only(false, true);
-	const KAA::filesystem::driver::mode sequential_binary_write_only(true, false);
-	const KAA::filesystem::driver::mode sequential_binary_read_write(true, true);
-
-	const KAA::filesystem::driver::share share_any_access;*/
-
+	// FUTURE: KAA: replace working_directory_session with SetUp()/TearDown().
 	class working_directory_session
 	{
 	public:
@@ -36,14 +28,43 @@ namespace
 
 using namespace KAA::filesystem;
 
+TEST(current_working_directory, get)
+{
+	// ARRANGE
+	const auto environment = std::make_unique<crt_directory_control>();
+	// ACT
+	const auto current_working_directory = environment->get_current_working_directory();
+	// ASSERT
+	ASSERT_FALSE(current_working_directory.empty());
+}
+
 TEST(current_working_directory, set)
 {
+	// ARRANGE
 	const auto environment = std::make_unique<crt_directory_control>();
 	const auto current_working_directory = environment->get_current_working_directory();
 	const auto new_working_directory = std::wstring { LR"(C:\Temp)" }; // FIX: KAA: must exists!
 
+	// ACT
 	const working_directory_session session { new_working_directory };
 
-	//environment->set_current_working_directory(new_working_directory); // FIX: KAA: auto restore!
-	EXPECT_EQ(new_working_directory, environment->get_current_working_directory());
+	// ASSERT
+	ASSERT_EQ(new_working_directory, environment->get_current_working_directory());
+}
+
+TEST(working_directory_session, sets_and_restores_current_working_directory)
+{
+	// ARRANGE
+	const auto environment = std::make_unique<crt_directory_control>();
+	const auto current_working_directory = environment->get_current_working_directory();
+
+	// ACT
+	{
+		const auto new_working_directory = std::wstring { LR"(C:\Temp)" }; // FIX: KAA: must exists!
+		const working_directory_session session { new_working_directory };
+		EXPECT_EQ(new_working_directory, environment->get_current_working_directory());
+	}
+
+	// ASSERT
+	ASSERT_EQ(current_working_directory, environment->get_current_working_directory());
 }
