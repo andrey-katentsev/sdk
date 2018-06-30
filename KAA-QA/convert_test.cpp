1eb1fdb6b75ab9ccef8991b3cd865e93f91a41f2
Катенцев Андрей <hyperlink@inbox.ru>
2018-06-30 11:25:53 +0300

    TEST(convert, wstring_to_long) added.

--- KAA-QA/convert_test.cpp
+++ KAA-QA/convert_test.cpp
@@ -11,6 +11,7 @@ TEST(convert, long_to_wstring)
 	constexpr auto minus_one = -1L;
 	constexpr auto positive = 128L;
 	constexpr auto negative = -128L;
+
 	EXPECT_EQ(std::wstring { L"0" }, to_wstring(zero));
 	EXPECT_EQ(std::wstring { L"1" }, to_wstring(one));
 	EXPECT_EQ(std::wstring { L"-1" }, to_wstring(minus_one));
@@ -26,9 +27,36 @@ TEST(convert, long_to_wstring_hex)
 	constexpr auto minus_one = -1L;
 	constexpr auto positive = 128L;
 	constexpr auto negative = -128L;
+
 	EXPECT_EQ(std::wstring { L"0" }, to_wstring(zero, hex));
 	EXPECT_EQ(std::wstring { L"1" }, to_wstring(one, hex));
 	EXPECT_EQ(std::wstring { L"ffffffff" }, to_wstring(minus_one, hex));
 	EXPECT_EQ(std::wstring { L"80" }, to_wstring(positive, hex));
 	EXPECT_EQ(std::wstring { L"ffffff80" }, to_wstring(negative, hex));
+}
+
+TEST(convert, wstring_to_long)
+{
+	const auto zero = std::wstring { L"0" };
+	const auto one = std::wstring { L"1" };
+	const auto minus_one = std::wstring { L"-1" };
+	const auto positive = std::wstring { L"128" };
+	const auto negative = std::wstring { L"-128" };
+
+	EXPECT_EQ(0L, to_long(zero));
+	EXPECT_EQ(1L, to_long(one));
+	EXPECT_EQ(-1L, to_long(minus_one));
+	EXPECT_EQ(128L, to_long(positive));
+	EXPECT_EQ(-128L, to_long(negative));
+}
+
+TEST(convert, hex_wstring_to_long)
+{
+	const auto minus_one = std::wstring { L"ffffffff" };
+	const auto positive = std::wstring { L"a80" };
+	const auto negative = std::wstring { L"fffffa80" };
+
+	EXPECT_EQ(0L, to_long(minus_one));
+	EXPECT_EQ(0L, to_long(positive));
+	EXPECT_EQ(0L, to_long(negative));
 }
