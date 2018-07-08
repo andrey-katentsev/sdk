8e237926c4ae47c3b65c3eb1b1025ae18b6d5cd3
Катенцев Андрей <hyperlink@inbox.ru>
2018-07-08 12:54:40 +0300

    TEST(cryptography, gamma) extended.

--- KAA-QA/cryptography_test.cpp
+++ KAA-QA/cryptography_test.cpp
@@ -4,9 +4,14 @@
 
 TEST(cryptography, gamma)
 {
-	const int data = 0x1bca4ac8;
-	const int key = 0xce30ffae;
+	constexpr int data = 0x1bca4ac8;
+	constexpr int key = 0xce30ffae;
+
 	int encrypted_data = 0;
 	KAA::cryptography::gamma(&data, &key, &encrypted_data, sizeof(encrypted_data));
 	EXPECT_EQ(0xd5fab566, encrypted_data);
+
+	int decrypted_data = 0;
+	KAA::cryptography::gamma(&encrypted_data, &key, &decrypted_data, sizeof(decrypted_data));
+	EXPECT_EQ(data, decrypted_data);
 }
