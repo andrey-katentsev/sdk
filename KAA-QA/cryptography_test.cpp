578ba93d80857f324bd67c72672e6bde0350eaa3
Катенцев Андрей <hyperlink@inbox.ru>
2018-07-14 15:22:10 +0300

    TEST(cryptography, protect_data) updated.

--- KAA-QA/cryptography_test.cpp
+++ KAA-QA/cryptography_test.cpp
@@ -19,10 +19,10 @@ TEST(cryptography, gamma)
 TEST(cryptography, protect_data)
 {
 	// {1EBCA742-1651-4559-B6BE-D4A20650895D}
-	constexpr GUID test_id = { 0x1ebca742, 0x1651, 0x4559, { 0xb6, 0xbe, 0xd4, 0xa2, 0x6, 0x50, 0x89, 0x5d } };
-
+	// constexpr GUID test_id = { 0x1ebca742, 0x1651, 0x4559, { 0xb6, 0xbe, 0xd4, 0xa2, 0x6, 0x50, 0x89, 0x5d } };
 	const std::vector<uint8_t> data = { 0xb6, 0xbe, 0xd4, 0xa2, 0x6, 0x50, 0x89, 0x5d };
 	const auto encrypted_data = KAA::cryptography::protect_data(&data[0], data.size());
+	EXPECT_NE(data, encrypted_data);
 	const auto decrypted_data = KAA::cryptography::unprotect_data(&encrypted_data[0], encrypted_data.size());
 	EXPECT_EQ(data, decrypted_data);
 }
