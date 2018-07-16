87358043c07507a4de388778dfb736363e634638
Катенцев Андрей <hyperlink@inbox.ru>
2018-07-16 21:35:01 +0300

    checksum_test.cpp added.

---
+++ KAA-QA/checksum_test.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+
+#include "../KAA/include/checksum.h"
+
+using namespace KAA::checksum;
+
+TEST(CRC32, IEEE)
+{
+	const auto polynomial = 0x04c11db7;
+	{
+		const auto data = std::vector<uint8_t>(1U, 0);
+		auto checksum = crc32(&data[0], data.size(), polynomial);
+		const auto benchmark = 0xfa60fb57;
+		EXPECT_EQ(benchmark, checksum);
+	}
+	{
+		const auto data = std::vector<uint8_t>(1U, 1);
+		auto checksum = crc32(&data[0], data.size(), polynomial);
+		const auto benchmark = 0xfc43cdc0;
+		EXPECT_EQ(benchmark, checksum);
+	}
+}
