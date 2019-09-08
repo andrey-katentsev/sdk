907d88bdffeb62c4963106e207b40f3e2ca13992
andrey-katentsev <hyperlink@inbox.ru>
2019-09-08 20:57:34 +0300

    KAA::checksum::append_crc32 function introduced.

--- KAA/source/checksum.cpp
+++ KAA/source/checksum.cpp
@@ -22,8 +22,25 @@ namespace KAA
 			{
 				checksum ^= static_cast<const uint8_t*>(data)[i];
 				for (register auto bit = 0; bit < 8; ++bit)
-				{
 					checksum = (checksum & 0x00000001) ? (checksum >> 1) xor polynomial : checksum >> 1;
+			}
+			return ~checksum;
+		}
+
+		// FUTURE: KAA: unreferenced (see polynomial).
+		uint32_t append_crc32(const uint8_t* data, size_t size, uint32_t checksum)
+		{
+			checksum = ~checksum;
+			while (size-- != 0)
+			{
+				unsigned long c = *data++;
+				for (unsigned long i = 0; i < 8; ++i)
+				{
+					if (((checksum ^ c) & 1) != 0)
+						checksum = (checksum >> 1) ^ 0xEDB88320;
+					else
+						checksum = (checksum >> 1);
+					c >>= 1;
 				}
 			}
 			return ~checksum;
