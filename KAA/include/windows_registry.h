fc16127ba28b8f6abbc12849f327807e2cb086a5
Андрей Катенцев <hyperlink@inbox.ru>
2019-09-17 21:23:16 +0300

    system::registry: std::auto_ptr replaced with the std::unique_ptr.

--- KAA/include/windows_registry.h
+++ KAA/include/windows_registry.h
@@ -11,8 +11,8 @@ namespace KAA
 		class windows_registry : public registry
 		{
 		private:
-			std::auto_ptr<registry_key> iopen_key(root_key section, const std::wstring& sub_key, const key_access& desired_access) override;
-			std::auto_ptr<registry_key> icreate_key(root_key section, const std::wstring& sub_key, key_options options, const key_access& desired_access, const void* desired_security) override;
+			std::unique_ptr<registry_key> iopen_key(root_key section, const std::wstring& sub_key, const key_access& desired_access) override;
+			std::unique_ptr<registry_key> icreate_key(root_key section, const std::wstring& sub_key, key_options options, const key_access& desired_access, const void* desired_security) override;
 		};
 	}
 }
--- KAA/source/registry.cpp
+++ KAA/source/registry.cpp
@@ -7,12 +7,12 @@ namespace KAA
 {
 	namespace system
 	{
-		std::auto_ptr<registry_key> registry::open_key(const root_key section, const std::wstring& sub_key, const key_access& desired_access)
+		std::unique_ptr<registry_key> registry::open_key(const root_key section, const std::wstring& sub_key, const key_access& desired_access)
 		{
 			return iopen_key(section, sub_key, desired_access);
 		}
 		
-		std::auto_ptr<registry_key> registry::create_key(const root_key section, const std::wstring& sub_key, const key_options options, const key_access& desired_access, const void* desired_security)
+		std::unique_ptr<registry_key> registry::create_key(const root_key section, const std::wstring& sub_key, const key_options options, const key_access& desired_access, const void* desired_security)
 		{
 			return icreate_key(section, sub_key, options, desired_access, desired_security);
 		}
