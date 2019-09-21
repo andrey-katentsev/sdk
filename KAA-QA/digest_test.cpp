77b1f445bc9434532a3c9431956b668ac2ac5e8b
Андрей Катенцев <hyperlink@inbox.ru>
2019-09-21 16:25:41 +0300

    'md5' test-case added.

--- KAA-QA/digest_test.cpp
+++ KAA-QA/digest_test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
 #include "../KAA/include/cryptography/md5.h"
+#include "../KAA/include/exception/windows_api_failure.h"
 
 using namespace KAA::cryptography;
 
@@ -25,4 +26,42 @@ TEST(md5, null)
 	const auto digest = calculate_md5(nullptr, 0);
 	const auto benchmark = md5_t { 0x04b2008fd98c1dd4, 0x7e42f8ec980980e9 };
 	EXPECT_EQ(benchmark, digest);
+}
+
+TEST(md5, empty)
+{
+	md5 digest;
+	constexpr auto benchmark = md5_t { 0x04b2008fd98c1dd4, 0x7e42f8ec980980e9 };
+	ASSERT_EQ(benchmark, digest.complete());
+}
+
+TEST(md5, constructor)
+{
+	md5 digest { {'m', 'd', '5'} };
+	constexpr auto benchmark = md5_t { 0x82ba23f6369bc21b, 0x1867b1d34f72f6aa };
+	ASSERT_EQ(benchmark, digest.complete());
+}
+
+TEST(md5, add_data)
+{
+	constexpr auto benchmark = md5_t { 0x82ba23f6369bc21b, 0x1867b1d34f72f6aa };
+	{
+		md5 digest;
+		digest.add_data({'m', 'd', '5'});
+		EXPECT_EQ(benchmark, digest.complete());
+	}
+	{
+		md5 digest;
+		digest.add_data({'m'});
+		digest.add_data({'d'});
+		digest.add_data({'5'});
+		EXPECT_EQ(benchmark, digest.complete());
+	}
+}
+
+TEST(md5, complete)
+{
+	md5 digest;
+	digest.complete();
+	EXPECT_THROW(digest.add_data({'m', 'd', '5'}), KAA::windows_api_failure);
 }
