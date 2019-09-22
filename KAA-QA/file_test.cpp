6b4d2598bb136f9506203421721afd7eb6e424d3
Андрей Катенцев <hyperlink@inbox.ru>
2019-09-22 11:40:40 +0300

    'filesystem' test case added.

--- KAA-QA/file_test.cpp
+++ KAA-QA/file_test.cpp
@@ -1,10 +1,13 @@
 #include "gtest/gtest.h"
 
+#include "../KAA/include/random.h"
 #include "../KAA/include/filesystem/crt_file_system.h"
+#include "../KAA/include/filesystem/filesystem.h"
 #include "../KAA/include/filesystem/path.h"
 
 namespace
 {
+	const KAA::filesystem::path::file none(L"./bin/none.bin");
 	const KAA::filesystem::path::file kibibyte(L"./bin/KiB.bin");
 
 	const KAA::filesystem::driver::mode read_attributes_only(false, false);
@@ -22,4 +25,27 @@ TEST(file, get_size)
 	crt_file_system filesystem;
 	auto file = filesystem.open_file(kibibyte, read_attributes_only, share_any_access);
 	EXPECT_EQ(1024U, file->get_size());
+}
+
+TEST(filesystem, file_exists)
+{
+	crt_file_system filesystem;
+	EXPECT_FALSE(file_exists(filesystem, none));
+	EXPECT_TRUE(file_exists(filesystem, kibibyte));
+}
+
+TEST(filesystem, get_file_size)
+{
+	crt_file_system filesystem;
+	EXPECT_EQ(1024U, get_file_size(filesystem, kibibyte));
+}
+
+TEST(filesystem, DISABLED_set_file_size)
+{
+	crt_file_system filesystem;
+	const auto size = KAA::cryptography::random() % (1024U + 1U);
+	const path::file empty = L"./bin/empty.bin";
+	EXPECT_NO_THROW(set_file_size(filesystem, empty, size));
+	ASSERT_EQ(size, get_file_size(filesystem, empty));
+	set_file_size(filesystem, empty, 0U);
 }
